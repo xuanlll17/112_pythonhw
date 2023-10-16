0453f4671440be661b9d2f39008af82b697da67e
HH <wlhs1726@gmail.com>
2023-10-16 22:52:14 +0800

    modify

--- 1016_作業/index2.py
+++ 1016_作業/index2.py
@@ -1,64 +1,102 @@
-import dataSource   #自訂module
+import yfinance as yf
 import tkinter as tk
 from tkinter import ttk
+from tkinter.simpledialog import Dialog
+from tkinter import messagebox
+import csv
 
-class Window(tk.Tk):    #繼承tk.TK
-    def __init__(self, **kwargs):   #**kwargs 轉換為引數名稱呼叫(沒有限定數量)    #父類別有default value時
-        #super() 執行父類別
-        super().__init__(**kwargs)  #super().__init__ 繼承父類別的__init__
-        self.title("鄉鎮人口統計")
-        self.configure(background='#81C7D4')    #設定背景色
+#抓股票
+data = yf.download("0050.TW", start='2023-01-01')
+data.to_csv("0050.csv")
 
-        topFrame = tk.Frame(self, background='#0089A7')
-        label = ttk.Label(topFrame,text="鄉鎮人口統計", font=('Helvetica', '30'))
-        label.pack(padx=20,pady=20)
-        topFrame.pack()
+#版面設定
+class Window(tk.Tk):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.title("小小股票介面")
 
-        bottomFrame = tk.Frame(self, background='#2E5C6E')
-        choices = dataSource.cityNames()
-        choicesvar = tk.StringVar(value=choices)
-        self.listbox = tk.Listbox(bottomFrame, listvariable=choicesvar, width=15)
-        self.listbox.pack(pady=20)
-        bottomFrame.pack(expand=True, fill='x')
-        self.listbox.bind("<<ListboxSelect>>", self.user_selected) #self.user_selected要有self,沒有會出錯
+#樹狀
+class Frame(ttk.LabelFrame):
+    def __init__(self,master,title, **kwargs):
+        super().__init__(master=master,text=title,**kwargs)
+        self.tree=ttk.Treeview(self,columns=["date","open","high","low","close","adjclose","volume"],show="headings")
+        self.tree.heading("date",text="日期")
+        self.tree.heading("open",text="開盤價")
+        self.tree.heading("high",text="最高價")
+        self.tree.heading("low",text="最低價")
+        self.tree.heading("close",text="收盤價")
+        self.tree.heading("adjclose",text="調整後收盤價")
+        self.tree.heading("volume",text="成交量")
 
-        #grid 表格
-        resultFrame = tk.Frame(self)
-        tk.Label(resultFrame,text="年度:").grid(column=0,row=0,sticky='E',pady=5)
-        tk.Label(resultFrame,text="地區:").grid(column=0,row=1,sticky='E',pady=5)
-        tk.Label(resultFrame,text="人口數:").grid(column=0,row=2,sticky='E',pady=5)
-        tk.Label(resultFrame,text="土地面積:").grid(column=0,row=3,sticky='E',pady=5)
-        tk.Label(resultFrame,text="人口密度:").grid(column=0,row=4,sticky='E',pady=5)
-        self.yearVar = tk.StringVar()
-        tk.Label(resultFrame,textvariable=self.yearVar).grid(column=1,row=0,sticky='W')
-        self.cityVar = tk.StringVar()
-        tk.Label(resultFrame,textvariable=self.cityVar).grid(column=1,row=1,sticky='W')
-        self.populationVar = tk.StringVar()
-        tk.Label(resultFrame,textvariable=self.populationVar).grid(column=1,row=2,sticky='W')
-        self.areaVar = tk.StringVar()
-        tk.Label(resultFrame,textvariable=self.areaVar).grid(column=1,row=3,sticky='W')
-        self.densityVar = tk.StringVar()
-        tk.Label(resultFrame,textvariable=self.densityVar).grid(column=1,row=4,sticky='W')
-        resultFrame.pack()
+        with open('0050.csv', 'r', encoding='UTF-8') as file:
+            data=csv.reader(file)
+            next(data)
+            text=[]
+            text.append([f"日期：{0}",f"開盤價：{1}",f"最高價：{2}",f"最低價：{3}"
+                        ,f"收盤價：{4}",f"調整後收盤價：{5}",f"成交量：{6}"])
+            
+            for t in data:
+                self.tree.insert("",tk.END,values=t)
+            self.tree.pack()
 
-    def user_selected(self, event):  
-        #event 名稱(可以更改) 點選listbox內容後會回傳值到event 
-        #沒設定,點擊listbox內容會出現錯誤
-        selectedIndex = self.listbox.curselection()[0] #抓出所有鄉鎮的資料存成tuple,[0]是抓出鄉鎮的名字顯示出來
-        cityName = self.listbox.get(selectedIndex) #抓出點擊鄉鎮的資料
-        datalist = dataSource.info(cityName)
-        self.yearVar.set(datalist[0])   
-        self.cityVar.set(datalist[1])
-        self.populationVar.set(datalist[2])
-        self.areaVar.set(datalist[3])
-        self.densityVar.set(datalist[4])
-        
-        
+        self.tree.bind("<<TreeviewSelect>>",self.item)
+
+        self.pack(expand=1,fill="both",padx=10,pady=20)
+    
+    def item(self,event):
+        item_id=self.tree.selection()[0]
+        item_dict=self.tree.item(item_id)
+
+    def choise(self):
+        print(self.user.get())
+
+
+#還沒辦法顯示
+class GetPassword(Dialog,Frame):
+    def body(self, master):
+        self.title(f"0050{Frame.date}資料")
+
+        data1=tk.Label(master, text='開盤價：').grid(row=0, sticky=tk.W)
+        data2=tk.Label(master, text='最高價：').grid(row=1, sticky=tk.W)
+        data3=tk.Label(master, text='最低價：').grid(row=2, sticky=tk.W)
+        data4=tk.Label(master, text='收盤價：').grid(row=3, sticky=tk.W)
+        data5=tk.Label(master, text='調整後收盤價：').grid(row=4, sticky=tk.W)
+        data6=tk.Label(master, text='成交量：').grid(row=5, sticky=tk.W)
+
+        data1.grid(row=0, column=1, sticky=tk.W)
+        data2.grid(row=1, column=1, sticky=tk.W)
+        data3.grid(row=2, column=1, sticky=tk.W)
+        data4.grid(row=3, column=1, sticky=tk.W)
+        data5.grid(row=4, column=1, sticky=tk.W)
+        data6.grid(row=5, column=1, sticky=tk.W)
+        self.pack(expand=1,fill="both",padx=10,pady=20)
+
+    #確認/取消鍵
+    def buttonbox(self):
+        box = tk.Frame(self)
+
+        w = tk.Button(box, text="確認", width=10, command=self.ok, default=tk.ACTIVE)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+        w = tk.Button(box, text="取消", width=10, command=self.cancel)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
+
+'''
+不知道這啥
+root = Tk()
+dialog = GetPassword(root)
+'''
+
+
 
 def main():
-   window = Window()
-   window.mainloop()
-
+    window=Window()
+    frame=Frame(window,"0050")
+    window.mainloop()
 
 if __name__ == "__main__":
-    main()  #呼叫main function
+    main()
