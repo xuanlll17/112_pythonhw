601a3a38293edb46b418fa633fba1f42254384bf
HH <wlhs1726@gmail.com>
2023-10-17 21:36:46 +0800

    modify

--- 1016_作業/index.py
+++ 1016_作業/index.py
@@ -2,7 +2,6 @@
 '''
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
 from tkinter.simpledialog import Dialog
 import csv
 
@@ -11,6 +10,7 @@ class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)        
         self.title("台積電股價")
+        self.myFrame = MyFrame(self,"台積電2023-01-01-2023-10-17")
 
 
 class MyFrame(tk.LabelFrame):
@@ -33,6 +33,8 @@ class MyFrame(tk.LabelFrame):
         self.tree.configure(yscrollcommand=self.scrollbar.set)
         
         #insert data---------------------------------------------------------------------
+        
+        
         with open('台積電.csv') as f:
             reader = csv.DictReader(f, delimiter=',')
             for row in reader:
@@ -57,48 +59,50 @@ class MyFrame(tk.LabelFrame):
         item_dict = self.tree.item(item_id)
         values = item_dict['values']
         print(values[0],values[1],values[2],values[3],values[4],values[5],values[6])
+        dialog = GetdataInfo(self,values)
+
+class GetdataInfo(Dialog):
+    def __init__(self, master, values,**kwargs):
+        self.values = values
+        super().__init__(master,**kwargs)
+
+    def body(self, master):
+        self.title("查看")
+
+        tk.Label(master, text='Date:').grid(row=0, sticky=tk.W)
+        tk.Label(master, text='Open:').grid(row=1, sticky=tk.W)
+        tk.Label(master, text='High:').grid(row=2, sticky=tk.W)
+        tk.Label(master, text='Low:').grid(row=3, sticky=tk.W)
+        tk.Label(master, text='Close:').grid(row=4, sticky=tk.W)
+        tk.Label(master, text='AdjClose:').grid(row=5, sticky=tk.W)
+        tk.Label(master, text='Volume:').grid(row=6, sticky=tk.W)
+        tk.Label(master, text=self.values[0]).grid(row=0,column=1, sticky=tk.E)
+        tk.Label(master, text=self.values[1]).grid(row=1,column=1, sticky=tk.E)
+        tk.Label(master, text=self.values[2]).grid(row=2,column=1, sticky=tk.E)
+        tk.Label(master, text=self.values[3]).grid(row=3,column=1, sticky=tk.E)
+        tk.Label(master, text=self.values[4]).grid(row=4,column=1, sticky=tk.E)
+        tk.Label(master, text=self.values[5]).grid(row=5,column=1, sticky=tk.E)
+        tk.Label(master, text=self.values[6]).grid(row=6,column=1, sticky=tk.E)
+
+    def buttonbox(self):
+
+        box = tk.Frame(self)
+
+        w = tk.Button(box, text="確認", width=10, command=self.ok, default=tk.ACTIVE)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+        w = tk.Button(box, text="取消", width=10, command=self.cancel)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
         
-        class GetdataInfo(Dialog):
-
-            def body(self, master):
-                self.title("查看")
-
-                tk.Label(master, text='Date:').grid(row=0, sticky=tk.W)
-                tk.Label(master, text='Open:').grid(row=1, sticky=tk.W)
-                tk.Label(master, text='High:').grid(row=2, sticky=tk.W)
-                tk.Label(master, text='Low:').grid(row=3, sticky=tk.W)
-                tk.Label(master, text='Close:').grid(row=4, sticky=tk.W)
-                tk.Label(master, text='AdjClose:').grid(row=5, sticky=tk.W)
-                tk.Label(master, text='Volume:').grid(row=6, sticky=tk.W)
-                tk.Label(master, text=values[0]).grid(row=0,column=1, sticky=tk.E)
-                tk.Label(master, text=values[1]).grid(row=1,column=1, sticky=tk.E)
-                tk.Label(master, text=values[2]).grid(row=2,column=1, sticky=tk.E)
-                tk.Label(master, text=values[3]).grid(row=3,column=1, sticky=tk.E)
-                tk.Label(master, text=values[4]).grid(row=4,column=1, sticky=tk.E)
-                tk.Label(master, text=values[5]).grid(row=5,column=1, sticky=tk.E)
-                tk.Label(master, text=values[6]).grid(row=6,column=1, sticky=tk.E)
-
-            def buttonbox(self):
-
-                box = tk.Frame(self)
-
-                w = tk.Button(box, text="確認", width=10, command=self.ok, default=tk.ACTIVE)
-                w.pack(side=tk.LEFT, padx=5, pady=5)
-                w = tk.Button(box, text="取消", width=10, command=self.cancel)
-                w.pack(side=tk.LEFT, padx=5, pady=5)
-
-                self.bind("<Return>", self.ok)
-                self.bind("<Escape>", self.cancel)
-
-                box.pack()
-        
-        dialog = GetdataInfo(self)
        
         
 
 def main():    
     window = Window()
-    myFrame = MyFrame(window,"台積電")
     window.mainloop()
 
 if __name__ == "__main__":
