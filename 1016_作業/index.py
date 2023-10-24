1cbf2dcbc4c82714a8451ca920e756498b4cbe10
Xuan <leeyitsuen0617@gmail.com>
2023-10-24 15:52:14 +0800

    modify

--- 1016_作業/index.py
+++ 1016_作業/index.py
@@ -99,43 +99,6 @@ class MyFrame(tk.LabelFrame):
         print(value[0],value[1],value[2],value[3],value[4],value[5],value[6])
         dialog = GetdataInfo(self,values=value)
         #self.dialog
-
-#Dialog-----------------------------------------------------------------------------------
-class GetdataInfo(Dialog):
-    def __init__(self, master, values,**kwargs): 
-        self.values = values
-        super().__init__(master,**kwargs)
-
-    def body(self, master):
-        self.title("查看")
-
-        tk.Label(master, text='Date:').grid(row=0, sticky=tk.W)
-        tk.Label(master, text='Open:').grid(row=1, sticky=tk.W)
-        tk.Label(master, text='High:').grid(row=2, sticky=tk.W)
-        tk.Label(master, text='Low:').grid(row=3, sticky=tk.W)
-        tk.Label(master, text='Close:').grid(row=4, sticky=tk.W)
-        tk.Label(master, text='AdjClose:').grid(row=5, sticky=tk.W)
-        tk.Label(master, text='Volume:').grid(row=6, sticky=tk.W)
-        tk.Label(master, text=self.values[0]).grid(row=0,column=1, sticky=tk.E)
-        tk.Label(master, text=self.values[1]).grid(row=1,column=1, sticky=tk.E)
-        tk.Label(master, text=self.values[2]).grid(row=2,column=1, sticky=tk.E)
-        tk.Label(master, text=self.values[3]).grid(row=3,column=1, sticky=tk.E)
-        tk.Label(master, text=self.values[4]).grid(row=4,column=1, sticky=tk.E)
-        tk.Label(master, text=self.values[5]).grid(row=5,column=1, sticky=tk.E)
-        tk.Label(master, text=self.values[6]).grid(row=6,column=1, sticky=tk.E)
-
-    def buttonbox(self):
-        box = tk.Frame(self)
-        w = tk.Button(box, text="確認", width=10, command=self.ok, default=tk.ACTIVE)
-        w.pack(side=tk.LEFT, padx=5, pady=5)
-        w = tk.Button(box, text="取消", width=10, command=self.cancel)
-        w.pack(side=tk.LEFT, padx=5, pady=5)
-
-        self.bind("<Return>", self.ok)
-        self.bind("<Escape>", self.cancel)
-
-        box.pack()
-        
        
         
 
